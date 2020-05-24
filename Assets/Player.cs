e523c9ec5865f06fa1fe7adef4069b214903c53f
Greg Stephen <GregAStephen@gmail.com>
2020-05-24 15:54:29 -0500

    got ship moving left and right

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(clampedPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
