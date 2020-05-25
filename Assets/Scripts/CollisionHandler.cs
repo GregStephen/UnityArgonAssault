7e3cf01586c3665a4e5a344f19e915178f1ab3df
Greg Stephen <GregAStephen@gmail.com>
2020-05-24 22:55:06 -0500

    Added the explosion and game reset on death

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathFX;
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         SendMessage("OnPlayerDeath");
+
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
